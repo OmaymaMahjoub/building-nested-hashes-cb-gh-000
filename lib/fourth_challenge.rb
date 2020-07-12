63403ca5fdf5a82208be4cd5bbf96b3e35782099
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-12 10:34:41 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,17 +4,14 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
-      ]
+      :hero_friends => [:friend_I=>{:name=>"Benvolio", age:"17", attitude: "worried"},
+      :friend_II=>{name:"Mercutio", age:"18", attitude:"hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
-      ]
+      :heroine_friends => [:friendI=>{name=>}]
    }
 }
 
